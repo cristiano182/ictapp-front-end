b4759e02647da2f7ed46caf8a663614814d85683
cristiano <cristiano182@gmail.com>
2019-03-28 00:23:15 -0300

    3

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -22,7 +22,7 @@ export default class Login extends Component {
           });
         } else {
           login(user.data);
-          window.location.href = 'ictapp.net/#/';
+          this.props.history.replace("/homeuser");
         }
       } catch (err) {
         this.setState({ error: err });
