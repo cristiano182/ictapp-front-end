8a3dac9b764d496c1d47c782baef409dd57f2df8
cristiano <cristiano182@gmail.com>
2019-03-29 17:15:56 -0300

    teste

--- src/Pages/Registrar/Registrar.js
+++ src/Pages/Registrar/Registrar.js
@@ -76,6 +76,7 @@ export default class Registrar extends Component {
             cursoName
           })
           .catch(err => console.log(err));
+          this.props.history.push('/login')
       } catch (err) {
         this.setState({ error: "Erro ao tentar registrar" + err });
       }
