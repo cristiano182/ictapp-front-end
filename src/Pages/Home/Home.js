8a5f0a69590937797540fb3cca58b1942eb31a1b
cristiano <cristiano182@gmail.com>
2019-05-03 10:59:07 -0300

    teste2

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-import bg from "../../images/bg.jpg"
-import bg2 from "../../images/bg2.png"
 import user from "../../images/user.png"
+import avatar from "../../images/avatar.png"
+import study from "../../images/study.jpg"
+import drive from "../../images/drive.png"
+import { Link } from "react-router-dom";
 export default class Home extends Component {
     constructor(props) {
         super(props);
@@ -13,7 +15,14 @@ export default class Home extends Component {
         return (
             <div >
                 <div style={{ margin: '0px', color: '#ddd', padding: '0px', paddingTop: "100px", height: '600px', display: 'flex', justifyContent: 'space-around', border: 'transparent', background: 'transparent' }}>
-                    <div style={{ textAlign: 'start' }} >  <span style={{ fontSize: "25px" }} > Compartilhe e consulte arquivos </span> <br /> Consulte ementa, carga-horária, pré-requisitos <br /> e outras informações  sobre disciplinas centradas em um só lugar.</div>
+                    <div style={{ textAlign: 'start' }} >  <span style={{ fontSize: "25px" }} > Informações sobre disciplinas </span> <br /> Consulte ementa, carga-horária, pré-requisitos <br /> e outras informações  sobre disciplinas centradas em um só lugar.
+                        < br />
+                        <Link className="btn btn-primary" to="/listagemucs"> Consultar</Link>
+                        <br /> <br />
+                        <span style={{ fontSize: "25px" }} > Compartilhe e consulte arquivos </span> <br />Contribua realizando upload de arquivos no google drive, sem limites e free.<br />
+                        <Link className="btn btn-primary" to="files"> Contribuir</Link>
+                    </div>
+
                     <div style={{
                         backgroundImage: 'url(' + user + ')',
                         backgroundSize: 'cover',
@@ -21,35 +30,76 @@ export default class Home extends Component {
                         backgroundRepeat: 'no-repeat',
                         border: 'transparent',
                         borderRadius: '20px',
-                         width: '700px', height: '490px'  }}></div>
+                        width: '700px', height: '490px'
+                    }}></div>
 
-            </div>
-            <div style={{ backgroundColor: 'white', margin: '0px', padding: '0px', height: '500px' }}>
-
-
-                <div style={{ display: 'flex', justifyContent: 'space-around', paddingTop: '80px' }}>
+                </div>
+                <div style={{ backgroundColor: 'white', margin: '0px', padding: '0px', height: '500px' }}>
 
 
-                <div><strong>Area Usuario</strong> <br /> <small>Acesse sua area de usuario, ajuste e acompanhe  < br />sua quantidade de horas complementares, disciplinas obrigatorias já cursadas e <br />lista de disciplinas interdisciplinares para BCT (Ciência e Tecnologia).</small></div>
+                    <div style={{ display: 'flex', justifyContent: 'space-around', paddingTop: '80px' }}>
 
-                <div><strong>Lista Disciplinas</strong> <br /><small>Lista de disciplinas com suas respectivas informações, pré-requisitos e ementa.</small> </div>
 
-                    <div> 
-                        <strong>Google Drive</strong> <br /> <small>
-                       Upload  ilimitado de arquivos.Compartilhe seus arquivos  <br />(listas, exercicios, livros em formato digital,notas de aulas e outros)  <br /> e acesse materiais separados por seção postados por outros alunos.</small>
+                        <div className="card" style={{ alignItems: 'center', border: 'transparent', background: 'white' }}>
+
+                            <div style={{
+                                padding: '0px',
+                                width: '70px',
+                                height: '70px',
+                                backgroundImage: 'url(' + avatar + ')',
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center center',
+                                backgroundRepeat: 'no-repeat',
+                            }}>
+                            </div> <br />
+                            <strong>Painel Usuario</strong>  <small>Acesse sua area de usuario, ajuste e acompanhe  < br />sua quantidade de horas complementares, disciplinas obrigatorias já cursadas e <br />lista de disciplinas interdisciplinares para BCT (Ciência e Tecnologia).</small>
+
+                        </div>
+
+                        <div className="card" style={{ alignItems: 'center', border: 'transparent', background: 'white' }}>
+                            <div style={{
+                                padding: '0px',
+                                width: '75px',
+                                height: '75px',
+                                backgroundImage: 'url(' + study + ')',
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center center',
+                                backgroundRepeat: 'no-repeat',
+                            }}>
+                            </div>
+                            <br />
+
+
+                            <strong>Lista Disciplinas</strong> <small>Lista de disciplinas com suas respectivas informações, pré-requisitos e ementa.</small> </div>
+
+                        <div className="card" style={{ alignItems: 'center', border: 'transparent', background: 'white' }}>
+
+                            <div style={{
+                                padding: '0px',
+                                width: '70px',
+                                height: '70px',
+                                backgroundImage: 'url(' + drive + ')',
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center center',
+                                backgroundRepeat: 'no-repeat',
+                            }}>
+                            </div>
+                            <br />
+
+
+                            <strong>Google Drive</strong> <small>
+                                Upload  ilimitado de arquivos.Compartilhe seus arquivos  <br />(listas, exercicios, livros em formato digital,notas de aulas e outros)  <br /> e acesse materiais separados por seção postados por outros alunos.</small>
+                        </div>
+
+
                     </div>
-                
-
-
-
-
+                </div>
+                <div className="footer" style={{ margin: '0px', padding: '0px', height: '200px', color: 'white' }}>
+                    <br /> <br /> <br /> <br /> <br /> <br />
+                    <span>© 2019-2020 ICTAPP. </span>
 
                 </div>
             </div>
-            <div className="footer" style={{ margin: '0px', padding: '0px', height: '200px' }}>
-
-            </div>
-            </div >
         );
     }
 
--- src/main/App/App.js
+++ src/main/App/App.js
@@ -3,18 +3,58 @@ import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import { Link } from "react-router-dom";
 import { isAuthenticated, logout } from "../../services/auth"
-
 import Routes from "../Routes/Routes";
-
+import ictapp from "../../images/ictapp.png"
+import { NavDropdown, Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
 
+
+        <Navbar sticky="top" expand="lg">
+
+        <Link 
+        style={{
+          marginLeft: '100px',
+          backgroundImage: 'url(' + ictapp + ')',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center center',
+          backgroundRepeat: 'no-repeat',
+           width: '50px', height: '50px'}} to="/"> </Link>
+    {  /* <Navbar.Brand href="/" style={{ marginLeft: '100px' }}>React-Bootstrap</Navbar.Brand> */ }
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+            </Nav>
+            <Form inline style={{ marginRight: '100px' }}>
+              <Nav.Link style={{ color: 'white', fontSize: '13px', padding: '0px', margin: '0px', textDecoration: 'none', width: "200px" }} href="/homeuser"><strong>Painel Usuario </strong></Nav.Link>
+              <Nav.Link style={{ color: 'white', fontSize: '13px', padding: '0px', margin: '0px', textDecoration: 'none', width: "200px" }} href="/listagemucs"><strong>Lista Disciplinas</strong></Nav.Link>
+              <Nav.Link style={{ color: 'white', fontSize: '13px', padding: '0px', margin: '0px', textDecoration: 'none', width: "200px" }} href="/files"><strong>Arquivos Google Drive</strong></Nav.Link>
+
+              {isAuthenticated() ?
+                <Nav.Link style={{ color: 'white', fontSize: '13px', padding: '0px', margin: '0px', textDecoration: 'none', width: "200px" }} href="/" onClick={e => logout()}><strong>Sair</strong></Nav.Link>
+                :
+                <Nav.Link style={{ color: 'white', fontSize: '13px', padding: '0px', margin: '0px', textDecoration: 'none', width: "200px" }} href="/login"><strong>Login</strong></Nav.Link>
+              }
+
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+
+
+
+
+
+
+
+
+        { /*
         <nav className="sticky-top" style={{ width: "100%", height: "40px", paddingTop: '20px',paddingBottom: '10px',display: "flex", justifyContent: 'flex-end', height: "100%" }}>
           <Link style={{ color: '#ddd', fontSize: '13px',padding: '0px',margin: '0px' ,textDecoration: 'none', width: "200px" }} className="" to="/"> <strong> Home</strong></Link>
-          <Link style={{ color: '#ddd', fontSize: '13px',padding: '0px',margin: '0px' ,textDecoration: 'none', width: "200px" }} className="" to="/homeuser"> <strong> Area do Usuario</strong></Link>
+          <Link style={{ color: '#ddd', fontSize: '13px',padding: '0px',margin: '0px' ,textDecoration: 'none', width: "200px" }} className="" to="/homeuser"> <strong> Painel Usuario</strong></Link>
           <Link style={{ color: '#ddd', fontSize: '13px',padding: '0px',margin: '0px' ,textDecoration: 'none', width: "200px" }} className="" to="/listagemucs"> <strong>Lista Disciplinas</strong></Link>
           <Link style={{ color: '#ddd', fontSize: '13px',padding: '0px',margin: '0px' ,textDecoration: 'none', width: "200px" }} className="" to="/files"> <strong>Arquivos Google Drive</strong></Link>
           {isAuthenticated() ?
@@ -23,7 +63,7 @@ export default class App extends Component {
             <Link style={{ color: '#ddd', fontSize: '13px',padding: '0px',margin: '0px' ,textDecoration: 'none', width: "200px" }} className="" to="/login"> <strong>Login</strong></Link>
           }
         </nav>
-
+        */}
 
         <Routes />
 
